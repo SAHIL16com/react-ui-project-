e5d6d576b7a13820dbab4109dd1bd898d26d7be5
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-07-01 20:43:28 +0530

    footer section completed

--- src/componants/Footer.jsx
+++ src/componants/Footer.jsx
@@ -6,9 +6,8 @@ const Footer = ({ theme }) => {
         <div className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4  sm:px-10 lg:px-24 xl:px-24' >
             <div className='flex justify-between lg:items-center max-lg:flex-col gap-10 '>
                 <div className='space-y-5 text-sm text-grey-700 dark:text-gray-300' >
-                    <img  src={ theme ==='dark' ? assets.logo_dark : assets.logo} alt="" className='w-32 sm:w-44' />
+                    <img src={theme === 'dark' ? assets.logo_dark : assets.logo} alt="" className='w-32 sm:w-44' />
                     <p className='max-w-md' > from strategy to execution , we craft digital solution that move your business forward </p>
-
                     <ul className='flex gap-8'>
                         <li> <a className='hover:text-primary' href="#home">Home</a></li>
                         <li> <a className='hover:text-primary' href="#services">Services</a></li>
@@ -16,9 +15,27 @@ const Footer = ({ theme }) => {
                         <li> <a className='hover:text-primary' href="#contact">Contact</a></li>
                     </ul>
                 </div>
-                <div></div>
+                <div className='text-grey-600 dark:text-400 '>
+                    <h3 className='font-semibold'>Subscribe to our newsletter</h3>
+                    <p className='text-sm mt-2 mb-6'>
+                        the latest news, articles, and resources, sent to your inbox weekly.
+                    </p>
+                    <div>
+                        <input type='email' placeholder='enter your email' className='w-full p-3 text-sm outline-none rounded dark:text-gray-200 bg-transparent border border-gray-300 dark:border-gray-600' />
+                        <button className='bg-primary text-white rounded px-6'>Subscribe</button>
+                    </div>
+                </div>
             </div>
-
+            <hr className='border-gray-300 dark:border-gray-600 my-6' />
+            <div className='pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap' >
+                <p>Copyright © 2026 Mishra.Company - All rights reserved.</p>
+                <div className='flex items-center justify-between gap-4 '>
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.instagram_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
